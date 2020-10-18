00aa9b6e9548aaf138ebd898a35604f94898466b
Tanasiuk <72519124+Tanasiuk@users.noreply.github.com>
2020-10-18 23:40:46 +0300

    Add files via upload

--- SecToDate.py
+++ SecToDate.py
@@ -1,11 +1,27 @@
-from datetime import datetime, timedelta
-
-def SecToDate():
-    sec = timedelta(seconds=int(input("Укажите количество секунд:\n")))
-    t = datetime(1,1,1) + sec
-    print("Дни:часы:минуты:секунды")
-    print("%d    %d    %d    %d" % (t.day-1, t.hour, t.minute, t.second))
+def SecToDate(sec):
+    oneyear = 31536000
+    if sec >= oneyear:
+        print ("Перевалили за год! Работаем с количеством секунду не больше года! (<=31535999)")
+    if sec < oneyear:
+        day = 86400
+        hour = 3600
+        minute = 60
+        resultday = 0
+        resulthour = 0
+        resultminute = 0
+        balancesec = sec
+        while day<=balancesec:
+            resultday+=1
+            balancesec-= day
+        while hour<=balancesec:
+            resulthour+=1
+            balancesec-= hour
+        while minute<=balancesec:
+            resultminute+=1
+            balancesec-= minute
+        print("Дни:часы:минуты:секунды")
+        print("%d    %d    %d    %d" % (resultday, resulthour, resultminute, balancesec))
 try:
-    SecToDate()
+    SecToDate(int(input("Укажите количество секунд:\n")))
 except ValueError:
-    print("Снова сломал?!")
+    print("Снова сломал?! Вводим количество секунд в цифрах! ")
